49fec962dbefaefe566b062cd45a659d1e07e3f0
RiccardoCataldi <74554002+RiccardoCataldi@users.noreply.github.com>
2024-02-18 19:43:55 +0100

    Update midi.py

--- midi.py
+++ midi.py
@@ -6,12 +6,10 @@ def print_message(msg):
     if msg.type == "note_on":
         ser.write(f"{msg.note}\n".encode())
     elif msg.type == "note_off":
-        ser.write(f"note_off {msg.note}\n".encode())  # Invia un messaggio distintivo per il note_off
+        ser.write(f"note_off {msg.note}\n".encode()) 
 
-# Apri la porta seriale
 ser = serial.Serial(port='COM3', baudrate=115200, timeout=.1) 
 
-# Continua a leggere i messaggi MIDI e inviarli alla porta seriale
 input_port_name = mido.get_input_names()[0]
 
 while True:
